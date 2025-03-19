d1a3757f567028f0e48d0af26d2f9ab39f42e8e2
WallisonLucas13 <lucaswalison136@gmail.com>
2025-03-19 12:03:06 -0300

    feat: aplica melhoria de thread safe no repository e async ao salvar transação

--- src/main/java/com/example/desafio/itau/repositories/TransactionRepository.java
+++ src/main/java/com/example/desafio/itau/repositories/TransactionRepository.java
@@ -3,20 +3,21 @@ package com.example.desafio.itau.repositories;
 import com.example.desafio.itau.models.TransactionModel;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Repository
 public class TransactionRepository {
 
-    private static List<TransactionModel> transactions = new ArrayList<>();
+    private final List<TransactionModel> transactions = new CopyOnWriteArrayList<>();
 
     public void save(TransactionModel transaction){
         transactions.add(transaction);
     }
 
     public List<TransactionModel> findAll(){
-        return transactions;
+        return Collections.unmodifiableList(transactions);
     }
 
     public void deleteAll(){
